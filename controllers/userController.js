3e95d3996e8533bc8927653af2c5d60cf26304b6
UnicornDad8 <cecibenitezca@gmail.com>
2025-12-29 13:27:07 -0300

    Add getUserConnections function to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -196,3 +196,22 @@ export const sendConnectionRequest = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 }
+
+export const getUserConnections = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await User.findById(userId).populate("connections followers following");
+
+    const connections = user.connections;
+    const followers = user.followers;
+    const following = user.following;
+
+    const pendingConnections = (await Connection.find({ to_user_id: userId, status: "pending" }).populate("from_user_id"))
+    .map(connection => connection.from_user_id);
+
+    res.json({ success: true, connections, followers, following, pendingConnections });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
